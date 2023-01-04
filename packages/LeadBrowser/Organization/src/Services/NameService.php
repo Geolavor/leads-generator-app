9711d627cf256af743c3e93202455b5d81bce971
Mariusz <mariuszmalek.dev@gmail.com>
2023-01-04 20:06:06 +0100

    prepare contacts

---
+++ packages/LeadBrowser/Organization/src/Services/NameService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LeadBrowser\Organization\Services;
+
+class NameServices
+{
+    /**
+     * @param string $name
+     * 
+     * @return string
+     */
+    protected function cleanName(string $name): string
+    {
+
+    }
+}

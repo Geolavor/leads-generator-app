6ade23cbe47c3a05335f9d70b21b562c80aec8f1
Mariusz <mariuszmalek.dev@gmail.com>
2023-01-06 21:26:14 +0100

    prepare integrations

--- config/services.php
+++ config/services.php
@@ -41,4 +41,33 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+
+    /**
+     * Integrations
+     */
+    'zoho' => [    
+        'client_id' => env('ZOHO_CLIENT_ID'),  
+        'client_secret' => env('ZOHO_CLIENT_SECRET'),  
+        'redirect' => env('ZOHO_REDIRECT_URI') 
+    ],
+    'pipedrive' => [    
+        'client_id' => env('PIPEDRIVE_CLIENT_ID'),  
+        'client_secret' => env('PIPEDRIVE_CLIENT_SECRET'),  
+        'redirect' => env('PIPEDRIVE_REDIRECT_URI') 
+    ],
+    'pipedrive' => [    
+        'client_id' => env('PIPEDRIVE_CLIENT_ID'),  
+        'client_secret' => env('PIPEDRIVE_CLIENT_SECRET'),  
+        'redirect' => env('PIPEDRIVE_REDIRECT_URI') 
+    ],
+    'intercom' => [    
+        'client_id' => env('INTERCOM_CLIENT_ID'),  
+        'client_secret' => env('INTERCOM_CLIENT_SECRET'),  
+        'redirect' => env('INTERCOM_REDIRECT_URI') 
+    ],
+    'salesforce' => [    
+        'client_id' => env('SALESFORCE_CLIENT_ID'),  
+        'client_secret' => env('SALESFORCE_CLIENT_SECRET'),  
+        'redirect' => env('SALESFORCE_REDIRECT_URI') 
+    ],
 ];
--- packages/LeadBrowser/Integrations/src/Providers/IntegrationsServiceProvider.php
+++ packages/LeadBrowser/Integrations/src/Providers/IntegrationsServiceProvider.php
@@ -15,6 +15,8 @@ class IntegrationsServiceProvider extends ServiceProvider
      */
     public function boot(Router $router)
     {
+        $this->app->register(EventServiceProvider::class);
+
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
     }
 
